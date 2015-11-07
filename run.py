949d6a1165378c7cfebe8be227222eb570631ac7
Arvanit <arvanitgrainca.ag@gmail.com>
2015-11-07 01:46:10 +0100

    Created database

--- run.py
+++ run.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for, request
+from pymongo import MongoClient
+data_base = MongoClient()
+mongo = data_base.tedhenat
+
 app = Flask(__name__)
 @app.route("/", methods=["GET"])
 def index():
 	return render_template("index.html")
+@app.route("/save", methods=["POST"])
+def save():
+	first_name = request.form["firstname"]
+	last_name = request.form["lastname"]
+	json_doc = {
+		"first_name": first_name,
+		"last_name":last_name
+	}
+	mongo.personi.insert(json_doc)
+
+	return redirect(url_for("index"))
 
 if __name__ == "__main__":
 	app.run(debug=True, host="0.0.0.0", port=5050)
